3f8ba690923ecdc0bc76ede0bf1b3ce6e48fcca9
Keith koki <kokikeith@gmail.com>
2024-01-23 11:24:40 +0000

    first commit

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Script for User Model
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    """
+    Class for user
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        """string representation"""
+        return f"User: id={self.id}"
